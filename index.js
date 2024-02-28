f395ee7b86a5fb09c3cba27aabf614ba748555d1
Vyacheslav-Belov-OO <vyacheslav.belov@onlyoffice.com>
2024-02-28 16:13:01 +0300

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const apiSecret = process.env.api_secret;
 app.use(express.json());
 
 
-app.post('/webhook', async (req, res) => {
+app.post('/deal', async (req, res) => {
     const event = req.body.event.replace('.', '_');
     const user_id = req.body.current['08f603bf9e0032d5a9f9e5cd39ca8c7a4374ac82'];
     console.log('new event from pipedrive', req.body);
@@ -41,6 +41,38 @@ app.post('/webhook', async (req, res) => {
         res.status(500).send('error');
     }
 });
+
+app.post('/person', async (req, res) => {
+    const event = req.body.event.replace('.', '_');
+    const user_id = req.body.current['1ab2463925590daaee2b572b68036cdfd9fec5f7'];
+    console.log('new event from pipedrive', req.body);
+    console.log('event_name_log',event);
+    console.log('user_id_log',user_id);
+    
+    const eventData = {
+        "client_id": `${user_id}`,
+        "events": [
+            {
+                "name": `${event}`,
+                "params": {
+                }
+            }
+        ]
+    };
+
+    
+    try {
+        const url = `https://www.google-analytics.com/mp/collect?measurement_id=${measurementId}&api_secret=${apiSecret}`;
+        const response = await axios.post(url, eventData);
+        console.log(eventData);
+        console.log('success', response.data);
+        res.status(200).send('success to GA4');
+    } catch (error) {
+        console.error('error', error);
+        res.status(500).send('error');
+    }
+});
+
 
 app.listen(port, () => {
     console.log(`server start on http://localhost:${port}`);
