6c86440b9fff48fcd5f02b3596d0abc614629954
Vyacheslav-Belov-OO <63102499+Vyacheslav-Belov-OO@users.noreply.github.com>
2024-05-31 16:20:18 +0300

    Update index.js

--- index.js
+++ index.js
@@ -11,8 +11,7 @@ const apiSecret = process.env.api_secret;
 app.use(express.json());
 
 
-app.post('/deal', async (req, res) => {
-    console.log(req);
+app.post('/deal', async (req, res) => {    
     const event = req.body.event.replace('.', '_');
     const user_id = req.body.current['08f603bf9e0032d5a9f9e5cd39ca8c7a4374ac82'];
     console.log('new event from pipedrive', req.body);
